213fa0d3ee272ef5a11605d9e668ddcd9b3d9b6e
Arron Atchison <arron.atchison@gmail.com>
2025-09-09 21:38:52 +0000

    initial config setup working

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -12,6 +12,7 @@
 import tb_pulumi
 import tb_pulumi.securityhub
 import tb_pulumi.guardduty
+import tb_pulumi.cfg
 # import tb_pulumi.network
 
 
@@ -22,6 +23,8 @@ project = tb_pulumi.ThunderbirdPulumiProject()
 
 # Pull the "resources" config mapping
 resources = project.config.get("resources")
+
+# Security Hub Configuration
 securityhub_account = resources["tb:securityhub:SecurityHubAccount"]
 security_hub_account_opts = securityhub_account["options"]
 security_hub_account = tb_pulumi.securityhub.SecurityHubAccount(
@@ -29,12 +32,20 @@ security_hub_account = tb_pulumi.securityhub.SecurityHubAccount(
     project,
 )
 
+# GuardDuty Configuration
 guardduty_configuration_opts = resources["tb:securityhub:GuardDutyConfiguration"]
 guardduty_configuration_features = guardduty_configuration_opts["features"]
-
 guardduty_configuration = tb_pulumi.guardduty.GuardDutyAccount(
     f"{project.name_prefix}",
     project,
     features=guardduty_configuration_features,
     # opts=guardduty_configuration_opts["opts"],
+)
+
+# AWS Config Configuration
+# aws_config_configuration_opts = resources["tb:securityhub:AWSConfigConfiguration"]
+aws_config_account = tb_pulumi.cfg.AwsConfigAccount(
+    f"{project.name_prefix}",
+    project,
+    # opts=aws_config_configuration_opts["opts"],
 )
