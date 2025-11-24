9a31def10d674a9a7bf53938f020809811c39aaa
Arron Atchison <arron.atchison@gmail.com>
2025-11-24 14:50:06 -0800

    Add CI/CD workglows using devcontainers for all deployment stacks (#3)
    * adding-validation workflow
    
    * disable change check for now
    
    * comment out optional mount lines
    
    * add package read permissions
    
    * add stack seclect and let's diff each change
    
    * need to inject access token secret
    
    * update upstram tb_pulumi branch
    
    * print the pulumi version
    
    * add ruff.toml
    
    * ruff format
    
    * rename secsphere to cloud-security-iac
    
    * rename stack from secsphere to cloud-security-iac
    
    * rename stack
    
    * let's reference stack name with the org/project/stack syntax as requessted.
    
    * consulidating iac runs again, seems that the dir only works once per run
    
    * let's export aws region env vars each stack run
    
    * let's configure an automation user for ci
    
    * ruff format
    
    * pulumi version bump
    
    * add merge for deploy test
    
    * typo
    
    * try again
    
    * auto approve
    
    * rename preview to deploy
    
    * no concurrency on deploy
    
    * detect changes and we are good to go!
    
    * only run on main merge
    
    * only diff if changes

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -13,6 +13,7 @@ import tb_pulumi
 import tb_pulumi.securityhub
 import tb_pulumi.guardduty
 import tb_pulumi.cfg
+import tb_pulumi.ci
 # import tb_pulumi.network
 
 
@@ -22,43 +23,54 @@ import tb_pulumi.cfg
 project = tb_pulumi.ThunderbirdPulumiProject()
 
 # Pull the "resources" config mapping
-resources = project.config.get("resources")
+resources = project.config.get('resources')
 
 # Security Hub Configuration
-securityhub_account = resources["tb:securityhub:SecurityHubAccount"]
-security_hub_account_opts = securityhub_account["options"]
+securityhub_account = resources['tb:securityhub:SecurityHubAccount']
+security_hub_account_opts = securityhub_account['options']
 security_hub_account = tb_pulumi.securityhub.SecurityHubAccount(
-    f"{project.name_prefix}",
+    f'{project.name_prefix}',
     project,
 )
 
 # GuardDuty Configuration
-guardduty_configuration_opts = resources["tb:securityhub:GuardDutyConfiguration"]
-guardduty_configuration_features = guardduty_configuration_opts["features"]
+guardduty_configuration_opts = resources['tb:securityhub:GuardDutyConfiguration']
+guardduty_configuration_features = guardduty_configuration_opts['features']
 guardduty_configuration = tb_pulumi.guardduty.GuardDutyAccount(
-    f"{project.name_prefix}",
+    f'{project.name_prefix}',
     project,
     features=guardduty_configuration_features,
     # opts=guardduty_configuration_opts["opts"],
 )
 
 # AWS Config Configuration
-aws_config_configuration = resources.get("tb:config:AwsConfigConfiguration")
+aws_config_configuration = resources.get('tb:config:AwsConfigConfiguration')
 # set delivery_email if present in config, otherwise None
-if aws_config_configuration and "delivery_email" in aws_config_configuration:
-    delivery_email = aws_config_configuration["delivery_email"]
+if aws_config_configuration and 'delivery_email' in aws_config_configuration:
+    delivery_email = aws_config_configuration['delivery_email']
 else:
     delivery_email = None
 
 # set aggregator_stack to bool value if present in config, otherwise False
-if aws_config_configuration and "aggregator_stack" in aws_config_configuration:
-    aggregator_stack = aws_config_configuration["aggregator_stack"]
+if aws_config_configuration and 'aggregator_stack' in aws_config_configuration:
+    aggregator_stack = aws_config_configuration['aggregator_stack']
 else:
     aggregator_stack = False
 
 aws_config_account = tb_pulumi.cfg.AwsConfigAccount(
-    f"{project.name_prefix}",
+    f'{project.name_prefix}',
     project,
     delivery_email=delivery_email,
     aggregator_stack=aggregator_stack,
+)
+
+# Set up an IAM user for automation purposes
+auto_users_opts = resources.get('tb:ci:AwsAutomationUser', {})
+for user, user_opts in auto_users_opts.items():
+    tb_pulumi.ci.AwsAutomationUser(f'{project.name_prefix}-{user}', project=project, **user_opts)
+
+# Set up IAM policies and groups to grant environment-bounded access to these resources
+sap = tb_pulumi.iam.StackAccessPolicies(
+    f'{project.name_prefix}-sap',
+    project=project,
 )
